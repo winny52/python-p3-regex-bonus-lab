abc3378fffc469a7addadc3630bb1115c90e0935
Winny <winny.chelangat2@student.moringaschool.com>
2023-09-12 09:33:45 +0300

    Completed lab

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,5 @@
 import re
 
-my_pattern = r""
+# Define the regular expression pattern
+my_pattern = r"(It's such a lovely day today\.|Some weather we're having today, huh\?|Maybe today's just not my day\.)"
 my_regex = re.compile(my_pattern)
-
